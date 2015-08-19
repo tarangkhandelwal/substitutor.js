bee1e12d93ba3b4e911a208a03ea91730073821d
ndembla <dembla.neeraj212@gmail.com>
2015-08-20 00:55:30 +0530

    shrink header on scroll

--- js/script.js
+++ js/script.js
@@ -28,3 +28,20 @@ console.log(jsonElement)
 
 
 });
+
+// Code for shrinking header on scroll
+$(window).scroll(function(){
+ var shrinkHeader = 70;
+  $(window).scroll(function() {
+    var scroll = getCurrentScroll();
+      if ( scroll >= shrinkHeader ) {
+           $('.navbar').addClass('shrink');
+        }
+        else {
+            $('.navbar').removeClass('shrink');
+        }
+  });
+function getCurrentScroll() {
+    return window.pageYOffset || document.documentElement.scrollTop;
+    }
+});
