3915c2d54c3c10eb465df7d64d67514c2fca31db
Tarang Khandelwal <tarangkhandelwal@gmail.com>
2015-08-22 22:06:30 +0530

    Added support for function Calls

--- js/script.js
+++ js/script.js
@@ -1,33 +1,12 @@
-isValidJSON=false;
+
 $("#substitutorBtn").click(function(){
-    if(!isValidJSON)
-        return;
     var rawString=$("#rawString").val();
-    var jsonString=$("#jsonToSubstitute").val();
- var result=substitutor(rawString, JSON.parse(jsonString));
+    var jsonString=$("#jsonToSubstitute").val();    
+    var result=substitutor(rawString, (new Function('return ' + jsonString))());
  $("#result").html(result);
 
 });
 
-
-$("#jsonToSubstitute").blur(function(){
-   var jsonElement=$("#jsonToSubstitute");
-   var jsonString=jsonElement.val();
-console.log(jsonElement)
-  try{
-    //check if valid JSON
-    JSON.parse(jsonString)
-    jsonElement.parent().removeClass("has-error");
-    jsonElement.parent().addClass("has-success");
-    isValidJSON=true;
-    }
-  catch(ex){
-    //invalid json
-    jsonElement.parent().addClass("has-error");
-  }
-
-
-});
 
 // Code for shrinking header on scroll
 $(window).scroll(function(){
